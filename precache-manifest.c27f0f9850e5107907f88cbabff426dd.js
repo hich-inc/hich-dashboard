a036f82c9f1fa8a6a4b4d47c98c24c1e18e49310
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2019-10-30 13:08:15 +0100

    Updates

---
+++ precache-manifest.c27f0f9850e5107907f88cbabff426dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbe1c31be3f90168fadfac6ff1c049a",
+    "url": "/hich-dashboard/index.html"
+  },
+  {
+    "revision": "a3d66851f39905a4dac0",
+    "url": "/hich-dashboard/static/css/2.0929a2c0.chunk.css"
+  },
+  {
+    "revision": "d529c3e3bd9da846f9d8",
+    "url": "/hich-dashboard/static/css/main.ce1e66d1.chunk.css"
+  },
+  {
+    "revision": "a3d66851f39905a4dac0",
+    "url": "/hich-dashboard/static/js/2.4c8ab8f5.chunk.js"
+  },
+  {
+    "revision": "d529c3e3bd9da846f9d8",
+    "url": "/hich-dashboard/static/js/main.506a7d84.chunk.js"
+  },
+  {
+    "revision": "b7b1391a660674da53e9",
+    "url": "/hich-dashboard/static/js/runtime~main.593d3e77.js"
+  },
+  {
+    "revision": "3aef1aa9401e08d8f5c9f8b3d151ac10",
+    "url": "/hich-dashboard/static/media/bg_analytics.3aef1aa9.jpg"
+  },
+  {
+    "revision": "8f2e2a3b3cd8d4bc36e428f80c83b115",
+    "url": "/hich-dashboard/static/media/bg_signup.8f2e2a3b.jpg"
+  }
+]);
